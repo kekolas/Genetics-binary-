e1100baae2b444c00cedd2c67b165e11f598744d
Furkan Atlı <mehmetfurkanatli@gmail.com>
2017-02-22 14:17:35 +0300

    yeni

--- GA-odev-bolum2/Form1.Designer.cs
+++ GA-odev-bolum2/Form1.Designer.cs
@@ -54,7 +54,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(417, 396);
+            this.ClientSize = new System.Drawing.Size(436, 410);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
